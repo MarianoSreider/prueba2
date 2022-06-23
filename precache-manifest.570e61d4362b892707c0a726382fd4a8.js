0590f6d54e7694a1311d82feb296df9d8f301c2d
marsre <mariano.a.sreider@gmail.com>
2022-06-23 13:59:50 -0300

    Updates

---
+++ precache-manifest.570e61d4362b892707c0a726382fd4a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cbd9e40f1d4c41c4ffeacb6774e482",
+    "url": "/prueba2/index.html"
+  },
+  {
+    "revision": "74671aec1ac2e76cb8db",
+    "url": "/prueba2/static/css/main.62d6cb19.chunk.css"
+  },
+  {
+    "revision": "30789e49f86f7ff34666",
+    "url": "/prueba2/static/js/2.72f11f41.chunk.js"
+  },
+  {
+    "revision": "e5bda5a21bc269a010b87b6cb029b6ee",
+    "url": "/prueba2/static/js/2.72f11f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74671aec1ac2e76cb8db",
+    "url": "/prueba2/static/js/main.ef30ae87.chunk.js"
+  },
+  {
+    "revision": "8fc77c4773d8b4ac7a3b",
+    "url": "/prueba2/static/js/runtime-main.b77fbbf1.js"
+  },
+  {
+    "revision": "82dc24198896d002062b83d196696de4",
+    "url": "/prueba2/static/media/Acc_Logo_All_White_RGB.82dc2419.svg"
+  },
+  {
+    "revision": "93cb83428bbb54d7ca14f0ed275f5592",
+    "url": "/prueba2/static/media/Graphik-Regular.93cb8342.ttf"
+  },
+  {
+    "revision": "937af1a1606e97838e2d44e5fde9a06a",
+    "url": "/prueba2/static/media/Graphik-Semibold.937af1a1.ttf"
+  },
+  {
+    "revision": "97208e43c03f17eae43bad74f463111b",
+    "url": "/prueba2/static/media/Home_page.97208e43.svg"
+  },
+  {
+    "revision": "792ea268efee83c48a7c3cc5a12c3732",
+    "url": "/prueba2/static/media/logo.792ea268.svg"
+  },
+  {
+    "revision": "111cc0e2cde4e9cb90468dfa5e6cfe85",
+    "url": "/prueba2/static/media/profile_Greg.111cc0e2.svg"
+  }
+]);
